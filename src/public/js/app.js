cd3f8c553955463f857ae954bdc0f7f6708d4ae5
prdg <prdg990515@gmail.com>
2023-03-05 19:18:34 +0900

    개인 채팅 서버 완성/ 주석제거전

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,6 +3,7 @@ const socket = io();
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
+const nickName = document.querySelector("#welcome #name");
 
 room.hidden = true;
 
@@ -16,11 +17,32 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = welcome.querySelector("#name input");
+  const value = input.value;
+  console.log(value);
+
+  socket.emit("nickname", value);
+}
+
 const showRoom = () => {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#msg");
+  msgForm.addEventListener("submit", handleMessageSubmit);
 };
 
 function handleRoomSubmit(event) {
@@ -34,9 +56,42 @@ function handleRoomSubmit(event) {
   input.value = "";
 }
 
-form.addEventListener("submit", handleRoomSubmit);
+function handleNameSubmit(event) {
+  event.preventDefault();
+  console.log("work");
+  const nameForm = welcome.querySelector("#name");
+  nameForm.addEventListener("submit", handleNicknameSubmit);
+}
 
-socket.on("welcome", () => {
+form.addEventListener("submit", handleRoomSubmit);
+nickName.addEventListener("submit", handleNameSubmit);
+
+socket.on("welcome", (user) => {
   //welcome이벤트를 받으면 함수를 실행시킴
-  addMessage("someone joined");
+  addMessage(`${user} joined`);
 });
+
+socket.on("bye", (left) => {
+  addMessage(`${left} left room`);
+});
+
+socket.on("new_message", addMessage);
+const roomList = welcome.querySelector("ul");
+socket.on("room_change", (rooms) => {
+  if (rooms.length === 0) {
+    roomList.innerHTML = "";
+    return;
+  }
+
+  rooms.forEach((room) => {
+    const li = document.createElement("li");
+    li.innerText = room;
+    roomList.append(li);
+  });
+});
+
+// rooms.forEach((value, key) => {
+//   if(sids.get(key) === undefined){
+//       console.log(key)
+//   }
+// })
--- src/server.js
+++ src/server.js
@@ -13,23 +13,86 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`listening on http://localhost:3000`);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    // if (sids.get(key) === undefined) {
+    publicRooms.push(key);
+    // }
+  });
+  return publicRooms;
+  // const sids = wsServer.sockets.adapter.sids;
+  // const rooms = wsServer.sockets.adapter.rooms
+}
+
 //emit 보내기
 //on 받기
 //http, ws 모두 작동
 const httpServer = http.createServer(app);
 const wsServer = SocketIo(httpServer);
+let userSocketId = [];
 
 wsServer.on("connection", (socket) => {
-  socket.onAny((event) => {
-    console.log(`socket Event:${event}`);
+  // socket["nickname"] = "Anon";
+  // socket.onAny((event) => {
+  //   console.log(`socket Event:${event}`);
+  // });
+  // socket.on("enter_room", (roomName, done) => {
+  //   //roomName이라는 방을 만들어 참가
+  //   socket.join(roomName);
+  //   //프론트에서 받은 함수를 프론트에서 실행(뭔가를 담아서 보낼수도있음)
+  //   done();
+  //   //roomName이라는방에 자신을 제외한 welcome이라는 이벤트를 받는 모두에게 보냄
+  //   socket.to(roomName).emit("welcome", socket.nickname);
+  //   wsServer.sockets.emit("room_change", publicRooms());
+  // });
+  // socket.on("disconnecting", () => {
+  //   socket.rooms.forEach((room) =>
+  //     socket.to(room).emit("bye", socket.nickname)
+  //   );
+  // });
+  // socket.on("disconnect", () => {
+  //   wsServer.sockets.emit("room_change", publicRooms());
+  // });
+  // socket.on("new_message", (msg, room, done) => {
+  //   socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+  //   done();
+  // });
+  // socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+  socket["nickname"] = "";
+
+  socket.on("nickName", (myId, id) => {
+    socket["nickname"] = `${myId}/${id}`;
+    let newUserSocketId = userSocketId.filter((i) => {
+      if (i.split("/")[0] === myId) {
+        return;
+      } else {
+        return i;
+      }
+    });
+    newUserSocketId.push(socket["nickname"]);
+    userSocketId = newUserSocketId;
+    console.log(userSocketId);
+    wsServer.sockets.emit("userId", userSocketId);
   });
-  socket.on("enter_room", (roomName, done) => {
-    //roomName이라는 방을 만들어 참가
+
+  socket.on("friendChat", (clickId, roomName) => {
+    console.log(clickId, roomName);
     socket.join(roomName);
-    //프론트에서 받은 함수를 프론트에서 실행(뭔가를 담아서 보낼수도있음)
-    done();
-    //roomName이라는방에 자신을 제외한 welcome이라는 이벤트를 받는 모두에게 보냄
-    socket.to(roomName).emit("welcome");
+    wsServer.in(clickId.split("/")[1].toString()).socketsJoin(roomName);
+    console.log("adddddaoter", wsServer.sockets.adapter);
+
+    // socket.emit("friendId", publicRooms());
+  });
+
+  socket.on("friendMessage", (newChat) => {
+    console.log(parseInt(newChat.roomId));
+    socket.to(parseInt(newChat.roomId)).emit("friendNew_message", newChat);
   });
 });
 
@@ -47,4 +110,4 @@ wsServer.on("connection", (socket) => {
 //   socket.send("hello@");
 // });
 
-httpServer.listen(3000, handleListen);
+httpServer.listen(4500, handleListen);
